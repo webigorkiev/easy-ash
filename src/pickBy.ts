6fd3ce6f9c3a2a51cd20e2b3ebdd70431aad6c00
Ihor Khomenko <webigorkiev@gmail.com>
2022-05-11 09:23:38 +0300

    update types for pullAll

--- src/pickBy.ts
+++ src/pickBy.ts
@@ -1,23 +1,17 @@
-declare type pickByHandler = (v: any, k: string) =>  boolean;
-
 /**
  * Pick some keys from object by handler
- * @param object
- * @param predicate function with two arguments (value, key)
- * @return {any}
  */
-export default (
-    object: Record<string, any>, 
-    predicate: pickByHandler = (v) => !!v
-) : Record<string, any> => {
-
-    if(object === null) return object;
-
+export default <T extends Record<string, any>, K extends keyof T>(
+    object: T,
+    predicate: (v: any, k: K) =>  boolean = (v) => !!v
+): Pick<T, K> => {
+    if(object === null) {
+        return object;
+    }
     const entries = Object
         .entries(object)
-        .filter(([k, v]) => predicate(v, k))
+        .filter(([k, v]) => predicate(v, k as K))
         .map(([k, v]) => ({ [k]: v }));
 
-    // @ts-ignore
-    return entries.length ? Object.assign(...entries) : {};
+    return (entries.length ? Object.assign({}, ...entries) : {}) as Pick<T, K>;
 }
